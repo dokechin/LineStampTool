d98a88de619b628035997308ffdcbd4f06a494fb
dokechin <user@user-PC.(none)>
2014-04-20 07:10:28 +0900

    change source

--- script/svg2png.pl
+++ script/svg2png.pl
@@ -5,16 +5,12 @@ use Archive::Zip;
 use Data::Dumper;
 
   my $rsvg = new Image::LibRSVG();
- my $formats       = Image::LibRSVG->getSupportedFormats();
-  print Dumper($formats);
   my @svgs = glob "./src/*.svg"; 
 
   for $svg(@svgs){
 
-    print $svg;
-    my $svg = basename($svg, '.svg');
-    my $png = sprintf("./dist/%s.png", $svg);
-    print $png;
+    my $svg_filename = basename($svg, '.svg');
+    my $png = sprintf("./dist/%s.png", $svg_filename);
 
     my ($x,$y) = (370,320);
     if ($svg eq "main"){
@@ -26,7 +22,8 @@ use Data::Dumper;
       $y = 74;
     }
 
-    $rsvg->convertAtSize("./src/01.svg", "./dist/01.png", $x, $y);
+    printf "convert $svg->$png\n", $svg, $png;
+    $rsvg->convertAtSize($svg, $png, $x, $y);
 
   }
 
